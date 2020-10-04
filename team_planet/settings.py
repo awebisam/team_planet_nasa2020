5e2406376649b6b8fc8885dabde7c7714f71a972
Aashish Bhandari <64518389+awebisam@users.noreply.github.com>
2020-10-04 19:03:08 +0545

    Update settings.py

--- team_planet/settings.py
+++ team_planet/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamplanet.nasa2020@gmail.com'
-EMAIL_HOST_PASSWORD = 'teamplanet12A'
+EMAIL_HOST_PASSWORD = 'SECRET'
